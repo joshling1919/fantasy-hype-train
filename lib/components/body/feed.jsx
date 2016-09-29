3e08d0c685a26349d5621ad3cfc7519b2133cab4
Joshua <joshling1919@gmail.com>
2016-09-29 01:18:29 -0700

    made basic skeleton for feed page

--- lib/components/body/feed.jsx
+++ lib/components/body/feed.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 
-
 class Feed extends React.Component {
 
-  customizedHeading(){
-    return(
-      "Devonta Freeman    Falcons Running Back"
-    );
+
+  componentDidMount(){
+    let acc = document.getElementsByClassName("accordion");
+
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].onclick = function() {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+      };
+    }
   }
 
   render(){
+
     return(
       <div>
         this is the feeds page
+        <button className="accordion">Section 1</button>
+        <div className="panel">
+            <p>Lorem ipsum...</p>
+          </div>
+
+        <button className="accordion">Section 2</button>
+        <div className="panel">
+            <p>Lorem ipsum...</p>
+          </div>
+
+        <button className="accordion">Section 3</button>
+        <div className="panel">
+            <p>Lorem ipsum...</p>
+          </div>
       </div>
     );
   }
