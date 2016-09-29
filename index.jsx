0755c773042df71e63334b8f46467f1e39a10f46
Joshua <joshling1919@gmail.com>
2016-09-29 16:03:58 -0700

    set up CORS

--- index.jsx
+++ index.jsx
@@ -5,10 +5,13 @@ import "./css/application.scss";
 import configureStore from './lib/store/store';
 import Root from './lib/root';
 
+import { fetchPlayers } from './lib/util/feed_api_util';
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  window.fetchPlayers = fetchPlayers;
   const root = document.getElementById('root');
   ReactDOM.render(
       <Root store={store}/>
